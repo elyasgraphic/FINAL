d58df769cda52e2c252ac2fe0dcf97a21d2a7104
elyasgraphic <eh655@drexel.edu>
2023-06-15 05:43:40 -0400

    ALL THESE CHANGES.I WANTED TO DIE

---
+++ deploy/js/scripts-min.js
@@ -0,0 +1,1 @@
+document.getElementById("burger").addEventListener("click",(function(){document.getElementById("header").classList.toggle("show-menu")}));
---
+++ dev/js/scripts.js
@@ -0,0 +1,3 @@
+document.getElementById("burger").addEventListener("click", function() {
+    document.getElementById("header").classList.toggle("show-menu");
+});
